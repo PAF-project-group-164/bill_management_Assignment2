486da8559f5c7dde4be1ef4aa661e989975a5983
poornima lakmal <88796670+lakmalpoornima@users.noreply.github.com>
2022-05-15 14:09:05 +0530

    Add files via upload

---
+++ sql/database.sql
@@ -0,0 +1,47 @@
+phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2022 at 07:32 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pafprojecteletricity`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billingapi`
+--
+
+CREATE TABLE IF NOT EXISTS `billingapi` (
+  `bid` int(6) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(200) NOT NULL,
+  `usage` varchar(200) NOT NULL,
+  `value` varchar(200) NOT NULL,
+  `vat` varchar(200) NOT NULL,
+  `total` varchar(200) NOT NULL,
+  PRIMARY KEY (`bid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+
+--
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
